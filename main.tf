486d0b020d5e8d2f201cec597b32d6c90068eaf8
ricoclesca <ricardo.clesca@icloud.com>
2024-06-13 01:02:31 -0400

    no_userdata

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_lightsail_instance" "gitlab_rico_test" {
   availability_zone = "us-east-1a"
   blueprint_id      = "amazon_linux_2"
   bundle_id         = "nano_3_0"
-  user_data         = file("script.sh")
+  # user_data         = file("script.sh")
   key_pair_name     = "week2"
   tags = {
     owner = "rico"
